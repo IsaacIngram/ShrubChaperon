3bf3db396907c76252f66c200386a5fb3f12a0c2
IsaacIngram <isaac@isaacingram.com>
2023-04-08 15:05:13 -0400

    Initial commit
    Initial commit of the secrets header file for users to define their
    secrets, such as network or server info

---
+++ main/secrets.h
@@ -0,0 +1,10 @@
+/*
+  Define all secrets (passwords, ports, hostnames, etc.) in this file
+*/
+
+#define WIFI_SSID "" ///< Network SSID
+#define WIFI_PASSWORD "" ///< Network password
+#define DD_AGENT_IP "" ///< IP of server serving the DataDog agent
+#define DD_AGENT_PORT "" ///< Port of server serving the DataDog agent
+#define METRIC_NAME "plant.moisture" ///< Name of the metric in DataDog
+
